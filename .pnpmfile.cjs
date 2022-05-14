2bb32f98ee0d326a7823b8e59372f9c9d2f960ac
Chuck Fan <fanck0605@qq.com>
2022-05-14 18:57:29 +0800

    不使用保留的 JS 关键词

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,13 +1,13 @@
-function readPackage(package, context) {
-    if (package.name === 'hexo-renderer-stylus') {
-        package.dependencies = {
-            ...package.dependencies,
+function readPackage(pkg, context) {
+    if (pkg.name === 'hexo-renderer-stylus') {
+        pkg.dependencies = {
+            ...pkg.dependencies,
             stylus: '0.54.5'
         }
         context.log('stylus@^0.54.8 => stylus@0.54.5 in dependencies of hexo-renderer-stylus@v2.0.0')
     }
 
-    return package
+    return pkg
 }
 
 module.exports = {
